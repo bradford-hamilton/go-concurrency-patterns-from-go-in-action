999b54be6cec2d52918bdff1f7e220ac4467bd76
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-11-10 12:33:46 -0700

    runner with example

---
+++ runner/runner-main-example.go
@@ -0,0 +1,46 @@
+// This sample program demonstrates how to use a channel to monitor the amount of
+// time the program is running and terminate the program if it runs too long.
+
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/bradford-hamilton/concurrency_patterns/runner"
+)
+
+// timeout is the number of seconds the program has to finish
+const timeout = 3 * time.Second
+
+func main() {
+	log.Println("Starting work.")
+
+	// Create a new timer value for this run.
+	r := runner.New(timeout)
+
+	// Add the tasks to be run.
+	r.Add(createTask(), createTask(), createTask())
+
+	// Run the tasks and handle the result.
+	if err := r.Start(); err != nil {
+		switch err {
+		case runner.ErrTimeout:
+			log.Println("Terminating due to timeout.")
+			os.Exit(1)
+		case runner.ErrInterrupt:
+			log.Println("Terminating due to interrupt")
+			os.Exit(2)
+		}
+	}
+
+	log.Println("Process ended.")
+}
+
+func createTask() func(int) {
+	return func(id int) {
+		log.Printf("Processor - Task #%d.", id)
+		time.Sleep(time.Duration(id) * time.Second)
+	}
+}
---
+++ runner/runner.go
@@ -0,0 +1,93 @@
+// Package runner is an example package from Go In Action: https://www.manning.com/books/go-in-action
+package runner
+
+import (
+	"errors"
+	"os"
+	"os/signal"
+	"time"
+)
+
+// Runner runs a set of tasks within a given timeout and can be shut down on
+// an operating system interrupt
+type Runner struct {
+	// interrupt channel reports a signal from the operating system
+	interrupt chan os.Signal
+
+	// complete channel reports that processing is done.
+	complete chan error
+
+	// timeout reports that time has run out.
+	timeout <-chan time.Time
+
+	// tasks holds a set of functions that are executed synchronously in index order.
+	tasks []func(int)
+}
+
+// ErrTimeout is returned when a value is received on the timeout.
+var ErrTimeout = errors.New("received timeout")
+
+// ErrInterrupt is returned when an event from the OS is received.
+var ErrInterrupt = errors.New("received interrupt")
+
+// New returns a new ready-to-use Runner
+func New(d time.Duration) *Runner {
+	return &Runner{
+		interrupt: make(chan os.Signal, 1),
+		complete:  make(chan error),
+		timeout:   time.After(d),
+	}
+}
+
+// Add attaches tasks to the Runner. A task is a function that takes an int ID.
+func (r *Runner) Add(tasks ...func(int)) {
+	r.tasks = append(r.tasks, tasks...)
+}
+
+// Start runs all tasks and monitors channel events.
+func (r *Runner) Start() error {
+	// We want to receive all interrupt based signals.
+	signal.Notify(r.interrupt, os.Interrupt)
+
+	// Run the different tasks on a different goroutine.
+	go func() {
+		r.complete <- r.run()
+	}()
+
+	select {
+	// Signaled when processing is done.
+	case err := <-r.complete:
+		return err
+		// Signaled when we run out of time.
+	case <-r.timeout:
+		return ErrTimeout
+	}
+}
+
+func (r *Runner) run() error {
+	for id, task := range r.tasks {
+		// Check for an interrupt signal from the OS.
+		if r.gotInterrupt() {
+			return ErrInterrupt
+		}
+
+		// Execute the registered task.
+		task(id)
+	}
+
+	return nil
+}
+
+func (r *Runner) gotInterrupt() bool {
+	select {
+	// Signaled when an interrupt even is sent.
+	case <-r.interrupt:
+		// Stop receiving any further signals.
+		signal.Stop(r.interrupt)
+		return true
+
+	// Continue running as normal
+	default:
+		return false
+	}
+}
